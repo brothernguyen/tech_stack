0ea3bf7c615a6676eaf231bb8497cb03415e3283
Anh Nguyen <tuananh3@gmail.com>
2018-06-22 17:00:52 +0700

    Removed all code to re-write

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
-import { Header } from './components/common';
-import LibraryList from './components/LibraryList';
 
-const App = () => {
-    return (
-        <Provider store={createStore(reducers)}>
-            <View style={{flex: 1}}>
-                <Header headerText='Tech Stack'/>
-                <LibraryList />
-            </View>
-        </Provider>        
+const App = () => (
+        <View />
     );
-    
-};
 
 export default App;
